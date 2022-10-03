16250300f6fd8a429f7f5728c2476f9f0af10f92
x-j-j <2849771625@qq.com>
2022-10-04 05:44:03 +0800

    firstcommit

---
+++ learning-maven/account-captcha/src/main/java/com/xjj/learning/maven/accountcaptcha/captchaMethod.java
@@ -0,0 +1,7 @@
+package com.xjj.learning.maven.accountcaptcha;
+
+public class captchaMethod {
+    public String returnCaptcha(){
+        return "captcha";
+    }
+}
---
+++ learning-maven/account-web/src/main/java/com/xjj/learning/maven/accountweb/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.xjj.learning.maven.accountweb;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AccountWebApplication.class);
+    }
+
+}
