603c236770e5bcdc2d3cfc117e2296fe782c5523
x-j-j <2849771625@qq.com>
2022-11-21 11:18:34 +0800

    添加红黑树实现

--- learning-java/util-concurrent/src/main/java/com/xjjlearning/java/util/concurrent/Source.java
+++ learning-java/util-concurrent/src/main/java/com/xjjlearning/java/util/concurrent/Source.java
@@ -10,15 +10,18 @@ import java.util.concurrent.atomic.LongAdder;
 public class Source {
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
+
+//        https://blog.csdn.net/weixin_43591980/article/details/116146008
         LongAdder longAdder = new LongAdder();
+
+//        https://csp1999.blog.csdn.net/article/details/116502499
         ConcurrentHashMap<String, String> concurrentHashMap = new ConcurrentHashMap<>(1);
 
         concurrentHashMap.put("a", "1");
         concurrentHashMap.put("b", "2");
 
-        System.out.println(1 / 2);
 
-
+//        https://tech.meituan.com/2016/11/18/disruptor.html
         // bounded, lock, arraylist
         ArrayBlockingQueue<Object> arrayBlockingQueue = new ArrayBlockingQueue<>(3);
 
